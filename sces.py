218858c8c4b7a824ac6edbc42e0488a5d2c2c7cd
yasminvoliveira <yasmin.v.oliveira6@edu.senai.br>
2026-05-29 13:29:20 -0300

    estrutura inicial do SCES

---
+++ sces.py
@@ -0,0 +1,18 @@
+## Estoque Simplificado
+
+
+def ID():
+    novoid = input
+
+
+
+
+Produtos = [
+    [ID,"","",""]
+]
+
+print("Vamos começar a organizar nosso estoque!")
+
+
+Produtos[0][0] = input("Escolha um ID para seu produto:")
+Produtos[0][1] = input("Escolha um ID para seu produto:")
